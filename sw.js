73807e3d54b9bf2b9285013686c0827c0059780c
aljwaal1 <yaya15112016@gmail.com>
2026-07-04 11:35:22 +0300

    Bump PWA cache for step learning flow

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='qamoosi-pwa-v3';
+const CACHE='qamoosi-pwa-v5';
 const ASSETS=['./','./index.html','./styles.css','./app.js','./v2-learning.js','./manifest.webmanifest','./assets/data/words.json','./icons/icon-192.svg','./icons/icon-512.svg'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
