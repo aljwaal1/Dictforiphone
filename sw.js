acf04818b563db21ccd64bb9816605dcea3897ad
aljwaal1 <yaya15112016@gmail.com>
2026-07-05 12:29:10 +0300

    Update PWA cache version

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='qamoosi-pwa-v5';
-const ASSETS=['./','./index.html','./styles.css','./app.js','./v2-learning.js','./manifest.webmanifest','./assets/data/words.json','./icons/icon-192.svg','./icons/icon-512.svg'];
+const CACHE='qamoosi-pwa-v6';
+const ASSETS=['./','./index.html','./styles.css','./excel-import.css','./app.js','./v2-learning.js','./excel-import.js','./manifest.webmanifest','./assets/data/words.json','./icons/icon-192.svg','./icons/icon-512.svg'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
 self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).then(r=>{let copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html')))));
